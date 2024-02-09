dfd983551d5e0c391e3e00a532bac53d2ced02f6
Shashank Bisht <105352161+Shashank-bisht@users.noreply.github.com>
2024-02-09 14:31:31 +0530

    google dummy

---
+++ src/components/SearchInput.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { IoMdClose } from "react-icons/io";
+import { useNavigate, useParams } from "react-router-dom";
+
+import MicIcon from "../assets/mic.svg";
+import ImageIcon from "../assets/image.svg";
+
+const SearchInput = () => {
+    const {query} = useParams();
+  const [searchQuery, setSearchQuery] = useState(query || "");
+  const navigate = useNavigate();
+  const searchQueryHandler = (event)=>{
+  if(event.key === "Enter" && searchQuery.length>0){
+    navigate(`/${searchQuery}/${1}`)
+  }
+}
+  return (
+    <div
+      id="searchBox"
+      className="h-[40px] w-full md:w-[540px] flex items-center gap-3 px-4 border border-[#dfe1e5] rounded-3xl hover:bg-white hover:shadow-c hover:border-0 focus-within:shadow-c focus-within:border-0"
+    >
+      <AiOutlineSearch size={18} color="#9aa0a6" />
+      <input
+        type="text"
+        onChange={(e) => setSearchQuery(e.target.value)}
+        onKeyUp={searchQueryHandler}
+        value={searchQuery}
+        autoFocus
+        className="h-full w-full focus:outline-none"
+        placeholder="Search Google or type a URL"
+      />
+
+      <div className="flex items-center gap-3">
+        {searchQuery && (
+          <IoMdClose
+            size={44}
+            className="cursor-pointer"
+            color="black"
+            onClick={() => setSearchQuery("")}
+          />
+        )}
+        <img className="h-6 w-6 cursor-pointer" src={MicIcon} alt="" />
+        <img className="h-6 w-6 cursor-pointer" src={ImageIcon} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default SearchInput;
